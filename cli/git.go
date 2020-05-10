5301b58ef976d98abccbe125dc0557d712f90888
Daniel Nery <danielnso97@gmail.com>
2020-05-10 14:13:19 -0300

    Start git command

--- cli/git.go
+++ cli/git.go
@@ -1,1 +1,21 @@
 package cli
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	gitCmd = &cobra.Command{
+		Use:   "git",
+		Short: "Utility for ThunderGit",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("NIY")
+		},
+	}
+)
+
+func init() {
+	rootCmd.AddCommand(gitCmd)
+}
